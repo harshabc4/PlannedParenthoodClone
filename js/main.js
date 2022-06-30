a482ed08c89c4a2339e7381edda4b3dae6511848
Brian Harsha <harshabc4@gmail.com>
2022-06-30 15:49:37 -0400

    begin dropdown animations

--- js/main.js
+++ js/main.js
@@ -13,4 +13,23 @@ window.addEventListener("scroll", () => {
 
    lastScrollY = window.scrollY;
 })
-// Hide scrollbar on scroll
+
+const learnBtn = document.querySelector('.learn-button');
+const deskDrop = document.querySelector('#LearnDesktopDropdown')
+let menuOpen = false;
+learnBtn.addEventListener('mouseover', () => {
+    if(!menuOpen) {
+        learnBtn.classList.add('open');
+        deskDrop.classList.remove('hidden')
+        deskDrop.classList.add('fade-in')
+        main.classList.add('repositioned')
+        // main.classList.add('ease-in')
+
+        menuOpen = true;
+    }else {
+        learnBtn.classList.remove('open');
+        deskDrop.classList.add('hidden')
+        main.classList.remove('repositioned')
+        menuOpen = false;
+    }
+})
